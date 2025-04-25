71e2a64f53952667dd0ea5fe4a012bebe85f7a3a
HanJiyao <hanjiyao0@gmail.com>
2025-04-25 13:40:25 +0800

    remove config again

--- src/lib/firebase/serverApp.ts
+++ src/lib/firebase/serverApp.ts
@@ -2,17 +2,8 @@ import "server-only";
 
 import { cookies } from "next/headers";
 import { initializeServerApp, initializeApp } from "firebase/app";
-
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from "@/lib/firebase/config";
 
 // Returns an authenticated client SDK instance for use in Server Side Rendering
 // and Static Site Generation
